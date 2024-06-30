3faa569065640957a855d8f3d3f6182e71e936f8
MiloCo86 <cpaez@pursuit.org>
2024-06-29 23:03:59 -0400

    PUT, DELETE, POST and GET unique Routes added

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -13,8 +13,38 @@ transactions.get('/', (req, res) => {
     res.json(transactionsArray)
 })
 
+//route to get a unique transaction
+transactions.get('/:idx', (req,res) =>{
+    const { idx } = req.params;
+    if(transactionsArray[idx]){
+        res.status(200).json(transactionsArray[idx]);
+    }else{
+        res.status(400).json({ error: "Transaction Not Found" })
+    }
+})
 
+//POST Route
+transactions.post('/',(req,res) =>{
+    transactionsArray.push(req.body)
+    res.json(transactionsArray[transactionsArray.length-1])
+})
 
+// PUT route
+transactions.put("/:idx", (req, res) => {
+    const { idx } = req.params
+    transactionsArray[idx] = req.body
+    res.status(200).json(transactionsArray[idx])
+})
 
+transactions.delete('/:idx', (req,res)=>{
+    const { idx } = req.params;
+    if(transactionsArray[idx]){
+        const deletedBookmark = transactionsArray.splice(idx,1)
+        res.json(transactionsArray[0])
+    }else{
+        res.status(404).json({error: "Transaction Not Found"})
+    }
+    
+})
 
 module.exports = transactions
---
+++ validations/transactionsValidations.js
@@ -0,0 +1,3 @@
+const checkInput = (req,res,next) =>{
+    
+}
